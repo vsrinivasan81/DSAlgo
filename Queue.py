e4b82f1f1642fed38411dc8a57fcbc60be8b6020
Vijay Srinivasan <cbevijay@gmail.com>
2020-05-17 19:59:08 +0530

    changed function names to avoid ambiguity with language function names

--- Queue.py
+++ Queue.py
@@ -2,37 +2,37 @@ class Queue(object):
     def __init__(self, head=None):
         self.storage = [head]
 
-    def enqueue(self, new_element):
+    def en_queue(self, new_element):
         self.storage.append(new_element)
 
     def peek(self):
         return self.storage[0]
 
-    def dequeue(self):
+    def de_queue(self):
         return self.storage.pop(0)
 
 
 # Setup
 q = Queue(1)
-q.enqueue(2)
-q.enqueue(3)
+q.en_queue(2)
+q.en_queue(3)
 
 # Test peek
 # Should be 1
 print(q.peek())
 
-# Test dequeue
+# Test de_queue
 # Should be 1
-print(q.dequeue())
+print(q.de_queue())
 
 # Test enqueue
-q.enqueue(4)
+q.en_queue(4)
 # Should be 2
-print(q.dequeue())
+print(q.de_queue())
 # Should be 3
-print(q.dequeue())
+print(q.de_queue())
 # Should be 4
-print(q.dequeue())
-q.enqueue(5)
+print(q.de_queue())
+q.en_queue(5)
 # Should be 5
 print(q.peek())
